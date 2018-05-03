9054cf9e0637a9ab4eac08e4b054f87c02393d1c
Josh Beckwith <josh.beckwith@gmail.com>
2018-05-02 21:53:08 -0700

    cleanup

--- scripts/script.js
+++ scripts/script.js
@@ -9,22 +9,7 @@ var plane0 = Scene.root.find('plane0')
 var plane1 = Scene.root.find('plane1')
 var text0 = Scene.root.find('text0')
 
-// TouchGestures.onTap(plane).subscribe(function(gesture){
-    // D.log(gesture.location)
-    // var hit = planeTracker.hitTest(gesture.location)
-    // D.log(hit.z)
-// })
-
-TouchGestures.onTap().subscribe(function(gesture) { 
-    // D.log(gesture.location.x)
-    
-    // var pixelPoint = Reactive.point2d(gesture.location.x, gesture.location.y)
-    // var camPosition = Scene.unprojectWithDepth(pixelPoint, .5)
-    // D.watch('cam.x', camPosition.x)
-    // D.watch('cam.y', camPosition.y)
-    // D.watch('cam.z', camPosition.z)
-})
-
+// target to track distance to
 var planePosition = Reactive.point(plane0.transform.x.lastValue, plane0.transform.y.lastValue, plane0.transform.z.lastValue)
 plane0.transform.position = planePosition
 D.watch('plane.x', planePosition.x)
@@ -32,15 +17,17 @@ D.watch('plane.y', planePosition.y)
 D.watch('plane.z', planePosition.z)
 
 var pixelPoint = Reactive.point2d(Reactive.val(0), Reactive.val(0))
+// unproject a short distance. zero doesn't work
 var camPosition = Scene.unprojectWithDepth(pixelPoint, 0.01)
 D.watch('cam.x', camPosition.x)
 D.watch('cam.y', camPosition.y)
 D.watch('cam.z', camPosition.z)
-// D.log(camPosition)
 
 var distance = camPosition.distance(planePosition).round()
 D.watch('distance', distance)
 
+// display distance
 text0.text = distance.toString()
 
+// unhide if within a distance
 plane1.hidden = distance.gt(300)
